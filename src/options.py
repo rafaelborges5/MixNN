eeac550f10f910b47a88bca59db172970dbbe8ab
Rafael Borges <R.BragaMedeirosMotaBorges@student.tudelft.nl>
2022-12-30 15:46:55 -0100

    feat: Marked the used options for later clean up

--- src/options.py
+++ src/options.py
@@ -10,22 +10,22 @@ def args_parser():
 
     # federated arguments (Notation for the arguments followed from paper)
     parser.add_argument('--epochs', type=int, default=10,
-                        help="number of rounds of training")
+                        help="number of rounds of training")  # used
     parser.add_argument('--num_users', type=int, default=100,
-                        help="number of users: K")
+                        help="number of users: K")  # used
     parser.add_argument('--frac', type=float, default=0.1,
-                        help='the fraction of clients: C')
+                        help='the fraction of clients: C')  # used
     parser.add_argument('--local_ep', type=int, default=10,
-                        help="the number of local epochs: E")
+                        help="the number of local epochs: E")  # used
     parser.add_argument('--local_bs', type=int, default=10,
-                        help="local batch size: B")
+                        help="local batch size: B")  # used
     parser.add_argument('--lr', type=float, default=0.01,
                         help='learning rate')
     parser.add_argument('--momentum', type=float, default=0.5,
                         help='SGD momentum (default: 0.5)')
 
     # model arguments
-    parser.add_argument('--model', type=str, default='mlp', help='model name')
+    parser.add_argument('--model', type=str, default='mlp', help='model name')  # used
     parser.add_argument('--kernel_num', type=int, default=9,
                         help='number of each kind of kernel')
     parser.add_argument('--kernel_sizes', type=str, default='3,4,5',
@@ -44,21 +44,21 @@ def args_parser():
 
     # other arguments
     parser.add_argument('--dataset', type=str, default='mnist', help="name \
-                        of dataset")
+                        of dataset")  # used
     parser.add_argument('--num_classes', type=int, default=10, help="number \
-                        of classes")
+                        of classes")  # used
     parser.add_argument('--gpu', default=None, help="To use cuda, set \
-                        to a specific GPU ID. Default set to use CPU.")
+                        to a specific GPU ID. Default set to use CPU.")  # used
     parser.add_argument('--optimizer', type=str, default='sgd', help="type \
                         of optimizer")
     parser.add_argument('--iid', type=int, default=1,
-                        help='Default set to IID. Set to 0 for non-IID.')
+                        help='Default set to IID. Set to 0 for non-IID.')  # used
     parser.add_argument('--unequal', type=int, default=0,
                         help='whether to use unequal data splits for  \
                         non-i.i.d setting (use 0 for equal splits)')
     parser.add_argument('--stopping_rounds', type=int, default=10,
                         help='rounds of early stopping')
-    parser.add_argument('--verbose', type=int, default=1, help='verbose')
+    parser.add_argument('--verbose', type=int, default=1, help='verbose')  # used
     parser.add_argument('--seed', type=int, default=1, help='random seed')
     args = parser.parse_args()
     return args
